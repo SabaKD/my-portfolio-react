3e2b6ac3ece0622e308902b825b6ceb7cc3eb269
SabaKD <saba.zadeh96@gmail.com>
2024-07-24 13:35:21 +0200

    appointment booking is added

--- src/Components/AppointmentBooking.js
+++ src/Components/AppointmentBooking.js
@@ -1,9 +1,22 @@
 import React from "react";
+import "./AppointmentBooking.css"
 
 export default function AppointmentBooking(){
     return(
-        <div>
-            Coming soon...
+        <div className="AppointmentBooking">
+            <div className="container">
+            <div className="giveMeACall">
+                <h2>
+                    Give me a call
+                </h2>
+                <p>
+                    Schedule a One-on-One 30 minutes call with me with me in Calendly
+                </p>
+                <a href="https://calendly.com/washingintroduction/30-mins-with-saba?month=2024-02"
+                    class=" btn primary-button" target="_blank">Schedule a call</a>
+
+            </div>
+        </div>
         </div>
     )
 }
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -3,7 +3,6 @@ import NavigationBar from "./NavigationBar";
 import Hero from './Hero';
 import UXProjects from './UXProjects';
 import FrontEndProjects from './FrontEndProjects';
-import Recommendations from './Recommendations';
 import AppointmentBooking from './AppointmentBooking';
 import NewFooter from './NewFooter';
 
@@ -14,7 +13,6 @@ export default function HomePage(){
             <Hero/>
             <UXProjects/>
             <FrontEndProjects/>
-            <Recommendations/>
             <AppointmentBooking/>
             <NewFooter/>
 
