0d2feb6fbc52a7d4a53aa43eae67ad3f351b6e45
SabaKD <saba.zadeh96@gmail.com>
2024-08-08 08:14:14 +0200

    added certificates partly

--- src/Components/AboutmeCertificates.js
+++ src/Components/AboutmeCertificates.js
@@ -2,16 +2,38 @@
 import React from "react";
 import AboutmeHandleCertificates from "./AboutmeHandleCertificates";
 import advui from "/Users/saba/Desktop/Shecodes/Portfolio/my-portfolio-react/src/Images/AdvanceUI.png";
+import funuxland from "../Images/UXLand.png";
+import figmeg from "../Images/FigmaMega.png";
+import introtocode from "../Images/IntroToCoding.png";
+
 
 
 export default function AboutmeCertificates(){
     const details=[
         {
             pic: advui,
-            name: "name",
-            institute: "institute",
-            duration: "duration"
-        }
+            name: "Advanced UI",
+            institute: "UX Land",
+            duration: "Six months"
+        },
+        {
+            pic: funuxland,
+            name: "Fundamental of UX and UX/UI in action",
+            institute: "UX Land",
+            duration: "Ten months"
+        },
+        {
+            pic: figmeg,
+            name: "UI/UX design beginner to expert",
+            institute: "Udemy",
+            duration: "24 total hours"
+        },
+        {
+            pic: introtocode,
+            name: "Introduction to coding",
+            institute: "SheCodes",
+            duration: "3 Weeks"
+        },
     ]
     return(
         <div className="AboutmeCertificates container">
